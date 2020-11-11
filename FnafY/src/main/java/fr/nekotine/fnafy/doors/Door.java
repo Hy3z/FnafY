012979f5bfc8069e4ea8190d4672b3988f64a5c2
XxGoldenbluexX <tonyg222@outlook.fr>
2020-11-11 13:02:28 +0100

    Variables for room and door class

--- FnafY/src/main/java/fr/nekotine/fnafy/doors/Door.java
+++ FnafY/src/main/java/fr/nekotine/fnafy/doors/Door.java
@@ -1,11 +1,41 @@
 package fr.nekotine.fnafy.doors;
 
+import org.bukkit.Location;
+
+import fr.nekotine.fnafy.room.Room;
+
 public class Door {
-	private DoorType doorType;
-	public Door(String doorName) {
+	
+	//VARS
+	private final Location AftonBlock;
+	private final Room room1;
+	private final Room room2;
+	
+	//CONSTRUCTEURS
+	
+	public Door(Room room1, Room room2, Location displayBlock) {
+		this.room1=room1;
+		this.room2=room2;
+		AftonBlock=displayBlock;
 		
 	}
-	public DoorType getType() {
-		return doorType;
+	
+	//FONCTIONS
+	
+	
+	
+	//GETTERS
+
+	public Room getRoom2() {
+		return room2;
 	}
+
+	public Room getRoom1() {
+		return room1;
+	}
+
+	public Location getAftonBlock() {
+		return AftonBlock;
+	}
+	
 }
--- FnafY/src/main/java/fr/nekotine/fnafy/room/Room.java
+++ FnafY/src/main/java/fr/nekotine/fnafy/room/Room.java
@@ -1,18 +1,82 @@
 package fr.nekotine.fnafy.room;
 
+import java.util.HashMap;
+import java.util.Set;
+
 import org.bukkit.Location;
 
+import fr.nekotine.fnafy.animation.ASOrderMP;
+import fr.nekotine.fnafy.enums.Animatronic;
+import fr.nekotine.fnafy.utils.BlockSelection;
+
 public class Room {
-	private Location camLocation;
-	private RoomType roomType;
-	public Room(String roomName ,RoomType type) {
-		
-	}
-	public Location getCameraLocation() {
-		return camLocation;
-	}
-	public RoomType getType() {
-		return roomType;
+	
+	//VARS
+	private final Location camLocation;
+	private final RoomType roomType;
+	private final BlockSelection AftonSurface;
+	private final BlockSelection AftonOutline;
+	private final BlockSelection MinimapOutline;
+	private final BlockSelection MinimapSurface;
+	private final String displayName;
+	private final HashMap<Animatronic,ASOrderMP> InRoomPoses=new HashMap<Animatronic,ASOrderMP>();
+	private final HashMap<Animatronic,ASOrderMP> MinimapPoses=new HashMap<Animatronic,ASOrderMP>();
+	
+	//CONSTRUCTEURS
+	
+	public Room(String roomName ,RoomType type,BlockSelection aftonsurf,BlockSelection aftonoutl,BlockSelection minmsurf,BlockSelection minmoutl,Location camloc,HashMap<Animatronic,ASOrderMP> rmpses,HashMap<Animatronic,ASOrderMP> aftses) {
+		displayName = roomName;
+		roomType = type;
+		AftonSurface = aftonsurf;
+		AftonOutline = aftonoutl;
+		MinimapSurface = minmsurf;
+		MinimapOutline = minmoutl;
+		camLocation = camloc;
+		InRoomPoses.putAll(rmpses);
+		MinimapPoses.putAll(aftses);
 	}
 	
+	//FONCTIONS
+	
+	public Set<Animatronic> getAllowedAnimatronics(){
+		return InRoomPoses.keySet();
+	}
+	
+	//GETTERS
+
+	public HashMap<Animatronic,ASOrderMP> getMinimapPoses() {
+		return MinimapPoses;
+	}
+
+	public HashMap<Animatronic,ASOrderMP> getInRoomPoses() {
+		return InRoomPoses;
+	}
+
+	public String getDisplayName() {
+		return displayName;
+	}
+
+	public BlockSelection getMinimapSurface() {
+		return MinimapSurface;
+	}
+
+	public BlockSelection getMinimapOutline() {
+		return MinimapOutline;
+	}
+
+	public BlockSelection getAftonOutline() {
+		return AftonOutline;
+	}
+
+	public BlockSelection getAftonSurface() {
+		return AftonSurface;
+	}
+
+	public RoomType getRoomType() {
+		return roomType;
+	}
+
+	public Location getCamLocation() {
+		return camLocation;
+	}
 }

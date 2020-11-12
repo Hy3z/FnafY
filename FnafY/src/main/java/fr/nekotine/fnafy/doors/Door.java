82ab1c9abefc2967c41cec92bfc446eb50bb3e04
XxGoldenbluexX <tonyg222@outlook.fr>
2020-11-12 09:49:53 +0100

    Adding armorStand poses for door and door minimap

--- FnafY/src/main/java/fr/nekotine/fnafy/doors/Door.java
+++ FnafY/src/main/java/fr/nekotine/fnafy/doors/Door.java
@@ -1,7 +1,11 @@
 package fr.nekotine.fnafy.doors;
 
+import java.util.HashMap;
+
 import org.bukkit.Location;
 
+import fr.nekotine.fnafy.animation.ASOrderMP;
+import fr.nekotine.fnafy.enums.Animatronic;
 import fr.nekotine.fnafy.room.Room;
 
 public class Door {
@@ -10,13 +14,17 @@ public class Door {
 	private final Location AftonBlock;
 	private final Room room1;
 	private final Room room2;
+	private final HashMap<Animatronic,ASOrderMP> InRoomPoses=new HashMap<Animatronic,ASOrderMP>();
+	private final HashMap<Animatronic,ASOrderMP> MinimapPoses=new HashMap<Animatronic,ASOrderMP>();
 	
 	//CONSTRUCTEURS
 	
-	public Door(Room room1, Room room2, Location displayBlock) {
+	public Door(Room room1, Room room2, Location displayBlock,HashMap<Animatronic,ASOrderMP> rmpses,HashMap<Animatronic,ASOrderMP> aftses) {
 		this.room1=room1;
 		this.room2=room2;
 		AftonBlock=displayBlock;
+		InRoomPoses.putAll(rmpses);
+		MinimapPoses.putAll(aftses);
 		
 	}
 	
@@ -36,6 +44,14 @@ public class Door {
 
 	public Location getAftonBlock() {
 		return AftonBlock;
+	}
+
+	public HashMap<Animatronic,ASOrderMP> getInRoomPoses() {
+		return InRoomPoses;
+	}
+
+	public HashMap<Animatronic,ASOrderMP> getMinimapPoses() {
+		return MinimapPoses;
 	}
 	
 }

1625198267ac7cdc219fe1ea8202b46957efd0ef
XxGoldenbluexX <tonyg222@outlook.fr>
2020-11-18 16:47:50 +0100

    changing Posture again....

--- FnafY/src/main/java/fr/nekotine/fnafy/animation/ASAnimOrder.java
+++ FnafY/src/main/java/fr/nekotine/fnafy/animation/ASAnimOrder.java
@@ -1,73 +1,33 @@
 package fr.nekotine.fnafy.animation;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.bukkit.Location;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause;
-import org.bukkit.util.EulerAngle;
+
+import fr.nekotine.fnafy.utils.Posture;
 
 public class ASAnimOrder {
 	
-	private final EulerAngle body;
-	private final EulerAngle leftArm;
-	private final EulerAngle rightArm;
-	private final EulerAngle leftLeg;
-	private final EulerAngle rightLeg;
-	private final EulerAngle head;
-	private final double x;
-	private final double y;
-	private final double z;
+	public final Posture pose;
+	public boolean relative;
 	
-	public ASAnimOrder(EulerAngle body, EulerAngle leftArm, EulerAngle rightArm, EulerAngle leftLeg, EulerAngle rightLeg, EulerAngle head, double x, double y, double z) {
-		this.body=body;
-		this.leftArm=leftArm;
-		this.rightArm=rightArm;
-		this.leftLeg=leftLeg;
-		this.rightLeg=rightLeg;
-		this.head=head;
-		this.x=x;
-		this.y=y;
-		this.z=z;
+	public ASAnimOrder(Posture pose,boolean relative) {
+		this.pose=pose;
+		this.relative=relative;
 	}
 	
-public void execute(ArmorStand as) {
-	double angle = Math.toRadians(as.getLocation().getYaw());
-	double cos = Math.cos(angle);
-	double sin = Math.sin(angle);
-	Location newLoc = as.getLocation().clone().add(x*cos+z*sin,y,x*sin+z*cos);
-	as.teleport(newLoc,TeleportCause.PLUGIN);
-	as.setBodyPose(body);
-	as.setLeftArmPose(leftArm);
-	as.setRightArmPose(rightArm);
-	as.setLeftLegPose(leftLeg);
-	as.setRightLegPose(rightLeg);
-	as.setHeadPose(head);
+	public void execute(ArmorStand as) {
+		double angle = Math.toRadians(as.getLocation().getYaw());
+		double cos = Math.cos(angle);
+		double sin = Math.sin(angle);
+		Location newLoc = as.getLocation().clone()/*.add(x*cos+z*sin,y,x*sin+z*cos*/;
+		as.teleport(newLoc,TeleportCause.PLUGIN);
+		as.setBodyPose(pose.body);
+		as.setLeftArmPose(pose.leftArm);
+		as.setRightArmPose(pose.rightArm);
+		as.setLeftLegPose(pose.leftLeg);
+		as.setRightLegPose(pose.rightLeg);
+		as.setHeadPose(pose.head);
 	}
-public Map<String, Object> serialize() {
-		HashMap<String, Object> serialized = new HashMap<String, Object>();
-		serialized.put("x", x);
-		serialized.put("y", y);
-		serialized.put("z", z);
-		serialized.put("body", body);
-		serialized.put("leftArm", leftArm);
-		serialized.put("rightArm", rightArm);
-		serialized.put("leftLeg", leftLeg);
-		serialized.put("rightLeg", rightLeg);
-		serialized.put("head", head);
-		return serialized;
-	}
-public static ASAnimOrder deserialize(Map<String, Object> args) {
-		EulerAngle eBody = (EulerAngle)args.get("body");
-		EulerAngle eLeftArm = (EulerAngle)args.get("leftArm");
-		EulerAngle eRightArm = (EulerAngle)args.get("rightArm");
-		EulerAngle eLeftLeg = (EulerAngle)args.get("leftLeg");
-		EulerAngle eRightLeg = (EulerAngle)args.get("rightLeg");
-		EulerAngle eHead = (EulerAngle)args.get("head");
-		double xx=(double)args.get("x");
-		double yy=(double)args.get("x");
-		double zz=(double)args.get("x");
-		return new ASAnimOrder(eBody, eLeftArm, eRightArm, eLeftLeg, eRightLeg, eHead, xx, yy, zz);
-	 }
+	
 }
--- FnafY/src/main/java/fr/nekotine/fnafy/utils/Posture.java
+++ FnafY/src/main/java/fr/nekotine/fnafy/utils/Posture.java
@@ -1,23 +1,49 @@
 package fr.nekotine.fnafy.utils;
 
-import org.bukkit.Location;
-import org.bukkit.entity.ArmorStand;
+import java.util.HashMap;
+import java.util.Map;
 
-import fr.nekotine.fnafy.animation.ASAnimOrder;
+import org.bukkit.Location;
+import org.bukkit.util.EulerAngle;
 
 public class Posture {
 	
-	private ASAnimOrder pose;
-	private Location loc;
+	public final EulerAngle body;
+	public final EulerAngle leftArm;
+	public final EulerAngle rightArm;
+	public final EulerAngle leftLeg;
+	public final EulerAngle rightLeg;
+	public final EulerAngle head;
+	public final Location location;
 	
-	public Posture (ASAnimOrder pose, Location location) {
-		this.pose = pose;
-		loc = location;
+	public Posture(EulerAngle body, EulerAngle leftArm, EulerAngle rightArm, EulerAngle leftLeg, EulerAngle rightLeg, EulerAngle head, Location loc) {
+		this.body=body;
+		this.leftArm=leftArm;
+		this.rightArm=rightArm;
+		this.leftLeg=leftLeg;
+		this.rightLeg=rightLeg;
+		this.head=head;
+		this.location=loc;
 	}
-	
-	public void apply(ArmorStand armorstand) {
-		armorstand.teleport(loc);
-		pose.execute(armorstand);
+	public Map<String, Object> serialize() {
+		HashMap<String, Object> serialized = new HashMap<String, Object>();
+		//TODO
+		serialized.put("body", body);
+		serialized.put("leftArm", leftArm);
+		serialized.put("rightArm", rightArm);
+		serialized.put("leftLeg", leftLeg);
+		serialized.put("rightLeg", rightLeg);
+		serialized.put("head", head);
+		return serialized;
 	}
-	
+	public static Posture deserialize(Map<String, Object> args) {
+		EulerAngle eBody = (EulerAngle)args.get("body");
+		EulerAngle eLeftArm = (EulerAngle)args.get("leftArm");
+		EulerAngle eRightArm = (EulerAngle)args.get("rightArm");
+		EulerAngle eLeftLeg = (EulerAngle)args.get("leftLeg");
+		EulerAngle eRightLeg = (EulerAngle)args.get("rightLeg");
+		EulerAngle eHead = (EulerAngle)args.get("head");
+		Location loc = null;//TODO
+		return new Posture(eBody, eLeftArm, eRightArm, eLeftLeg, eRightLeg, eHead, loc);
+	 }
 }

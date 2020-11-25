f843510c5decda1d28c3c2f47b79c8312cee8c96
XxGoldenbluexX <tonyg222@outlook.fr>
2020-11-25 16:37:53 +0100

    import fix

--- FnafY/src/main/java/fr/nekotine/fnafy/commands/ComAnim.java
+++ FnafY/src/main/java/fr/nekotine/fnafy/commands/ComAnim.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.EnumSet;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Set;
 import java.util.function.Predicate;

2af71db729183e23d9b0ad26b5998e35feb0dbd8
XxGoldenbluexX <tonyg222@outlook.fr>
2020-12-24 14:10:04 +0100

    fixing ref in taskManager

--- FnafY/src/main/java/fr/nekotine/fnafy/task/TaskManager.java
+++ FnafY/src/main/java/fr/nekotine/fnafy/task/TaskManager.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Random;
 
 import org.bukkit.ChatColor;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 

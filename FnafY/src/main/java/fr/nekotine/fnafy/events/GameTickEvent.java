599218a30ddc4e16fbb34be7e637aa94ed047bac
XxGoldenbluexX <tonyg222@outlook.fr>
2021-01-03 21:08:24 +0100

    jsp

--- FnafY/src/main/java/fr/nekotine/fnafy/events/GameTickEvent.java
+++ FnafY/src/main/java/fr/nekotine/fnafy/events/GameTickEvent.java
@@ -4,9 +4,11 @@ import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
 public class GameTickEvent extends Event{
-	private static final HandlerList HANDLERS_LIST = new HandlerList();
+	
+	private static final HandlerList handlers = new HandlerList();
+
 	@Override
 	public HandlerList getHandlers() {
-		return HANDLERS_LIST;
+		return handlers;
 	}
 }
